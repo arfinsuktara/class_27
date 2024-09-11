6a0529ed979aa4a82a1c086417de9a255a678f4e
SUK TARA <suktara447@gmail.com>
2024-09-11 12:05:48 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+//function calculation(){
+    let x = 10
+    let y = 10
+    let z = x + y
+    let result = z
+   console.log(result);{
+    
+}
+calculation()
+
+// Array
+let teams = ["suktara","marjia","munia","dipa","nipa","eshan"]
+console.log(teams);
+Array
+
+// Array(for loop)
+let teams1 = ["suktara","marjia","munia","dipa","nipa","eshan"]
+for (let i = 0; i < teams1.length; i++){
+    console.log(teams1[i]);
+}
+// Array(for loop)
+
+// Array map
+let teams2 = ["suktara","marjia","munia","dipa","nipa","eshan"]
+teams2.map((item)=>{
+    console.log(item);
+
+})
+
+// Array map
+
+// web speech syntisis api mdn
+let utterance = new SpeechSynthesisUtterance("Hello world!");
+speechSynthesis.speak(utterance);
+// web speech syntisis api mdn
+
